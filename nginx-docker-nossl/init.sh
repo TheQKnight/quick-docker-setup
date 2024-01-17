8e6f0c67031e12a03a08f7e92e0aaf901a9b1bb8
Elijah Lipsky <elijah@vitrical.com>
2024-01-16 20:22:42 -0800

    Removed nginx

--- nginx-docker-nossl/init.sh
+++ nginx-docker-nossl/init.sh
@@ -49,11 +49,8 @@ then
       exit 1
 fi
 
-# Update and install nginx
-
 sudo apt-get update -y
 sudo apt-get install docker-compose -y
-sudo apt-get install nginx -y
 
 # Setup firewall
 
@@ -61,22 +58,11 @@ sudo ufw allow OpenSSH
 sudo ufw allow ssh
 sudo ufw allow http
 sudo ufw allow https
-sudo ufw allow 'Nginx HTTP'
-sudo ufw allow 'Nginx HTTPS'
 sudo ufw --force enable
 
 sed -i "s/REGISTRY_NAME/$REGISTRY_NAME/g" docker-compose.yml
 sed -i "s/REGISTRY_IMAGE/$REGISTRY_IMAGE/g" docker-compose.yml
 sed -i "s/REGISTRY_TAG/$REGISTRY_TAG/g" docker-compose.yml
-
-# Copy nginx config
-
-cp nginx/nginx.conf /etc/nginx/nginx.conf
-cp nginx/sites-available/default /etc/nginx/sites-available/default
-
-# Restart nginx
-
-sudo systemctl restart nginx
 
 # Login to docker
 
